a52bfe37cb300290520b57c5001be7d824f1b637
Frank-Sin99 <56038484+Frank-Sin99@users.noreply.github.com>
2021-04-21 13:57:26 +0200

    Add files via upload

---
+++ ChatbotWidget-main4/static/js/lib/index.js
@@ -0,0 +1,15 @@
+
+/* module for importing other js files */
+function include(file) {
+    const script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+
+    document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+include('https://cdn.jsdelivr.net/gh/Frank-Sin99/test@main/ChatbotWidget-main4/static/js/lib/jquery.min.js');
+include('https://cdn.jsdelivr.net/gh/Frank-Sin99/test@main/ChatbotWidget-main4/static/js/lib/materialize.min.js');
+include('https://cdn.jsdelivr.net/gh/Frank-Sin99/test@main/ChatbotWidget-main4/static/js/lib/chart.min.js');
+include('https://cdn.jsdelivr.net/gh/Frank-Sin99/test@main/ChatbotWidget-main4/static/js/lib/uuid.min.js');
